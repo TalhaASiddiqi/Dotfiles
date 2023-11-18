0c0ee084dbe3b1bde74f7790b6481e03796d586f
Talha Siddiqi <talha.siddiqi@educative.io>
2023-11-18 21:40:34 +0500

    updated nvim

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -1,7 +1,19 @@
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
 return {
   options = {
     opt = { fillchars = { diff = "╱" } },
-    g = { copilot_no_tab_map = true, copilot_assume_mapped = true },
+    g = {
+      copilot_no_tab_map = true,
+      copilot_assume_mapped = true,
+    },
   },
   lsp = {
     setup_handlers = {
@@ -30,15 +42,18 @@ return {
         })
 
         local languages = {
+          -- javascript = { eslint, prettier },
+          -- javascriptreact = { eslint, prettier },
           typescript = { eslint, prettier },
           typescriptreact = { eslint, prettier },
           lua = { stylua },
           python = { isort, black, pylint },
         }
+
         local efmls_config = {
           filetypes = vim.tbl_keys(languages),
           settings = { rootMarkers = { ".git/" }, languages = languages },
-          root_dir = require("lspconfig.util").find_git_ancestor,
+          root_dir = require("lspconfig.util").root_pattern("tscofig.json", "yarn.lock", "package.json", ".git"),
           init_options = {
             documentFormatting = true,
             documentRangeFormatting = true,
@@ -50,37 +65,42 @@ return {
           on_attach = require("astronvim.utils.lsp").on_attach,
           cmd = {
             "efm-langserver",
-            "-loglevel",
-            "10",
-            "-logfile",
-            "/Users/talha/Desktop/efm.log",
+            -- "-loglevel",
+            -- "10",
+            -- "-logfile",
+            -- "/Users/talha/Desktop/efm.log",
           },
         }))
       end,
     },
     config = {
-      pylsp = {
-        root_dir = function(arg) return require("lspconfig.util").find_git_ancestor(arg) end,
-        settings = {
-          pylsp = {
-            configurationSources = {},
-            plugins = {
-              pyflakes = { enabled = false },
-              mccabe = { enabled = false },
-              pylint = { enabled = false },
-              yapf = { enabled = false },
-              autopep8 = { enabled = false },
-              flake8 = { enabled = false },
-              pycodestyle = { enabled = false },
-            },
-          },
-        },
-      },
+      -- pylsp = {
+      --   root_dir = function(arg) return require("lspconfig.util").find_git_ancestor(arg) end,
+      --   settings = {
+      --     pylsp = {
+      --       configurationSources = {},
+      --       plugins = {
+      --         pyflakes = { enabled = false },
+      --         mccabe = { enabled = false },
+      --         pylint = { enabled = false },
+      --         yapf = { enabled = false },
+      --         autopep8 = { enabled = false },
+      --         flake8 = { enabled = false },
+      --         pycodestyle = { enabled = false },
+      --       },
+      --     },
+      --   },
+      -- },
     },
-    formatting = { async = true, disabled = { "pylsp", "tsserver" }, timeout_ms = 50000 },
+    formatting = {
+      async = true,
+      disabled = { "pylsp", "tsserver" },
+      timeout_ms = 50000,
+    },
   },
   plugins = {
     { "mason.nvim", opts = { PATH = "append" } },
+    { "lbrayner/vim-rzip", event = "BufEnter" },
     {
       "ggandor/leap.nvim",
       lazy = false,
@@ -90,23 +110,55 @@ return {
       "creativenull/efmls-configs-nvim",
       dependencies = { "neovim/nvim-lspconfig" },
     },
+    { "null-ls.nvim", enabled = false },
     {
       "sindrets/diffview.nvim",
       lazy = false,
       opts = {
         default_args = { DiffviewOpen = { "--imply-local" } },
         enhanced_diff_hl = true,
+        view = { merge_tool = { layout = "diff4_mixed" } },
       },
-    },
+    }, --[[ {
+            "samoshkin/vim-mergetool",
+            lazy = false,
+            config = function()
+                vim.g.mergetool_layout = "bmr"
+                require("vim-mergetool").setup()
+            end
+        } ]]
     {
       "catppuccin/nvim",
       name = "catppuccin",
       priority = 1000,
-      opts = { transparent_background = true, term_colors = true },
+      opts = {
+        transparent_background = true,
+        integrations = {
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+          rainbow_delimiters = true,
+        },
+      },
     },
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     { "github/copilot.vim", event = "InsertEnter", cmd = "Copilot" },
-    { "HiPhish/rainbow-delimiters.nvim", lazy = false },
+    {
+      "HiPhish/rainbow-delimiters.nvim",
+      event = "BufEnter",
+      setup = function()
+        local rainbow_delimiters = require "rainbow-delimiters"
+        require("rainbow-delimiters.setup").setup { [""] = rainbow_delimiters.strategy["local"] }
+      end,
+    },
+    {
+      "indent-blankline.nvim",
+      opts = {
+        indent = { char = "▏" },
+        scope = { char = "▎", highlight = highlight },
+      },
+    },
   },
   colorscheme = "catppuccin-mocha",
   mappings = {
